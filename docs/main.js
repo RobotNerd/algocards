4654a401aa2549842171efb35844dd5094823d26
Marshall Bowles <marshall@marshallbowles.com>
2019-04-26 12:14:22 -0700

    Randomize order only on page load

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,32 @@
+// Order in which the questions are displayed.
+// Contains indicies in algorithms array.
+let order = [];
+
+// Index in the order array of the currently displayed question.
+let current = 0;
+
+/** Build a random order for showing questions. */
+function generateOrder() {
+  // see: https://stackoverflow.com/a/12646864/241025
+  order = [...Array(algorithms.length).keys()];
+  for (var i = order.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = order[i];
+    order[i] = order[j];
+    order[j] = temp;
+  }
+}
+
 function getAnswerElement() {
   return document.getElementById('answer');
+}
+
+function getNextIndex() {
+  current++;
+  if (current >= algorithms.length) {
+    current = 0;
+  }
+  return current;
 }
 
 function getQuestionElement() {
@@ -9,10 +36,15 @@ function getQuestionElement() {
 function loadQuestion() {
   let answer = getAnswerElement();
   let question = getQuestionElement();
-  let index = Math.floor(Math.random() * algorithms.length);
+  let index = getNextIndex();
   answer.style.display = "none";
   answer.innerHTML = algorithms[index].answer;
   question.innerHTML = algorithms[index].question;
+}
+
+function onPageLoad() {
+  generateOrder();
+  loadQuestion();
 }
 
 function nextQuestion() {
